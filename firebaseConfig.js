7292d64f74d88b43d50788e6aaf30f933ba73440
catto2006 <2fastcat@gmail.com>
2025-07-24 18:55:34 +0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyAjhr9440jqm7xk4TISB9LXlU7UMfJ-aYY",
+      authDomain: "umama-study-portal.firebaseapp.com",
+      projectId: "umama-study-portal",
+      storageBucket: "umama-study-portal.firebasestorage.app",
+      messagingSenderId: "438854993441",
+      appId: "1:438854993441:web:bbe2003382a6bfefbcc560",
+    };
+
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
